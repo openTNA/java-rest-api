6296f2db8271e1d15de30e265126f3afaa021b3d
jamesho.tw <jamesho.tw@gmail.com>
2018-03-08 18:17:10 +0800

    Add entity model classes

---
+++ src/main/java/org/opentna/data/model/entity/Attendance.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opentna.data.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Model for attendance records.
+ *
+ * @author James Ho
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+    name = "attendance_records",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "card_id", "logged_unix"})
+)
+public class Attendance implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, nullable = false)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+  @JsonIdentityReference(alwaysAsId = true)
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "card_id", nullable = false)
+  @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "serialNo")
+  @JsonIdentityReference(alwaysAsId = true)
+  private ProximityCard proximityCard;
+
+  @Column(name = "logged_unix", nullable = false)
+  private Long loggedAt;
+
+  @Column(name = "created_unix", nullable = false)
+  private Long createdAt;
+
+}
---
+++ src/main/java/org/opentna/data/model/entity/User.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opentna.data.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.opentna.data.model.BaseEntity;
+
+/**
+ * Model for users.
+ *
+ * @author James Ho
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+@JsonIgnoreProperties(value = {"password"})
+public class User extends BaseEntity {
+
+  @Column(name = "name", unique = true, nullable = false, length = 32)
+  private String username;
+
+  @Column(name = "secret", nullable = false, length = 512)
+  private String password;
+
+  @Type(type = "org.hibernate.type.BooleanType")
+  @Column(name = "must_change_secret", nullable = false)
+  private boolean mustChangePassword;
+
+  @Type(type = "org.hibernate.type.BooleanType")
+  @Column(name = "is_active", nullable = false)
+  private boolean enabled;
+
+  @ManyToMany(targetEntity = Role.class, cascade = CascadeType.ALL)
+  @JoinTable(
+      name = "related_users_roles",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"),
+      uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "role_id"})
+  )
+  private Set<Role> roles;
+
+  @ManyToMany(targetEntity = ProximityCard.class, cascade = CascadeType.ALL)
+  @JoinTable(
+      name = "related_users_proximity_cards",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "card_id", unique = true),
+      uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "card_id"})
+  )
+  private Set<ProximityCard> proximityCards;
+
+}
